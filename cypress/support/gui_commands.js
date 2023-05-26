bd65f5da0210a6e6761c796b19a931c3eb388932
José Arilson <40409188+josearilson@users.noreply.github.com>
2023-05-26 15:58:52 -0300

    Update gui_commands.js

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -2,7 +2,6 @@
 Cypress.Commands.add('login', (nome, senha) => {
     cy.get('input[formcontrolname = "userName"]').type(nome)
     cy.get('input[formcontrolname="password"]').type(senha, { log: false })//nao mostra a senha com o comando logFalse
-
 })
 
 
@@ -10,24 +9,12 @@ Cypress.Commands.add('usuarioDisponivel', (nome) => {
     cy.contains('a', 'Register now').click();
     cy.get('input[formcontrolname="userName"]').type(nome);
     cy.contains('button', 'Register').click();
-
-
-
-
 });
 
-
 Cypress.Commands.add('Registra_Usuario', (email,) => {
-
     cy.contains('a', 'Register now').click();
     cy.get('input[formcontrolname = "email"]').type(email);
     cy.contains('button', 'Register').click();
-
-
-
-
-
-
 });
 
 
@@ -37,8 +24,5 @@ Cypress.Commands.add('Email_fullName', (email, fullName) => {
 });
 
 Cypress.Commands.add('LoginValid', (email, senha) => {
-
-
-
 
 })
